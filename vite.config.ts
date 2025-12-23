5d1ae5c2bba17138e18519105c2c7b27c0a02a3b
mkmail1103 <mkmail1103@icloud.com>
2025-12-23 13:06:21 +0900

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  base: './',
-  build: {
-    outDir: 'dist',
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.', '');
+  return {
+    plugins: [react()],
+    base: './',
+    build: {
+      outDir: 'dist',
+    },
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.API_KEY),
+    },
+  };
 });
